24ef5c702f19a4c72a2d0ba09006ef69204ff071
wrolanda <nikita.mitriackov@gmail.com>
2022-10-13 16:10:26 +0300

    loader upgraded for multiple requests at the same time

--- src/app/interceptors/network-requests.interceptor.ts
+++ src/app/interceptors/network-requests.interceptor.ts
@@ -10,16 +10,29 @@ import { LoadingService } from '../services/loading.service';
 
 @Injectable()
 export class NetworkRequestsInterceptor implements HttpInterceptor {
+  totalRequests = 0;
+  completedRequests = 0;
+
   constructor(private loadingService: LoadingService) {}
 
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
+
     this.loadingService.show();
+    this.totalRequests++;
+
     return next.handle(request).pipe(
-      finalize(() => { 
-      this.loadingService.hide();
+      finalize(() => {
+        
+        this.completedRequests++;
+
+        if (this.completedRequests === this.totalRequests) {
+          this.loadingService.hide();
+          this.completedRequests = 0;
+          this.totalRequests = 0;
+        }
       })
     );
   }
